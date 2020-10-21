70cbc6d5511848574bc62864a91a790e6b552b30
SriramTummala <sriramtummala87@gmail.com>
2020-10-20 19:04:47 -0500

    Initial commit

---
+++ src/main/java/com/javadb/aws/config/DynamoDBConfig.java
@@ -0,0 +1,29 @@
+package com.javadb.aws.config;
+
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+    public static final String SERVICE_ENDPOINT = "yourendpoint";
+    public static final String REGION = "yourregion";
+    public static final String ACCESS_KEY ="youraccessket";
+    public static final String SECRET_KEY = "yoursecretkey";
+    @Bean
+    public DynamoDBMapper mapper(){
+        return new DynamoDBMapper(amazonDynamoDBConfig());
+    }
+
+    private AmazonDynamoDB amazonDynamoDBConfig() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY,SECRET_KEY))).build();
+    }
+}
